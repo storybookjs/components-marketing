b4c3c00c79e34effc270f60921cee8fc06da6ecb
winkerVSbecks <varunvachhar@gmail.com>
2022-09-08 11:09:23 -0400

    better defaults for links-context

--- src/components/links-context.ts
+++ src/components/links-context.ts
@@ -23,18 +23,18 @@ export interface Links {
 }
 
 export const defaultLinks = {
-  home: { url: '/' },
-  whyStorybook: { url: '/why' },
+  home: { url: 'https://storybook.js.org/' },
+  whyStorybook: { url: 'https://storybook.js.org/docs/react/why' },
   componentDriven: { url: 'https://componentdriven.org' },
-  guides: { url: '/docs' },
+  guides: { url: 'https://storybook.js.org/docs' },
   tutorials: { url: 'https://storybook.js.org/tutorials' },
-  changelog: { url: 'https://storybook.js.org/changelog' },
+  changelog: { url: 'https://storybook.js.org/releases' },
   telemetry: { url: 'https://storybook.js.org/telemetry' },
   showcase: { url: 'https://storybook.js.org/showcase' },
   projects: { url: 'https://storybook.js.org/showcase/projects' },
   componentGlossary: { url: 'https://storybook.js.org/showcase/glossary' },
-  integrations: { url: '/integrations' },
-  getInvolved: { url: '/get-involved' },
+  integrations: { url: 'https://storybook.js.org/addons' },
+  getInvolved: { url: 'https://storybook.js.org/community' },
   blog: { url: 'https://storybook.js.org/blog' },
   hiring: { url: 'https://www.chromatic.com/company/jobs' },
 };
