8d3d55d1026f6a3221c5ae77138a1befcb1dd2a9
Kyle Gach <kyle.gach@gmail.com>
2023-05-31 15:46:52 -0600

    Update to latest SB 7
    - Clean up `.storybook/main.js`
    - Rename `Intro.stories.mdx` -> `Intro.mdx`
        - Update `stories` globs

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,32 +1,37 @@
-module.exports = {
-  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.(tsx|js)'],
-  staticDirs: ['./static'],
-  addons: ['@storybook/addon-essentials', '@storybook/addon-a11y', 'storybook-addon-pseudo-states', '@storybook/addon-interactions'],
-  features: {
-    interactionsDebugger: true
-  },
-  core: {},
-  webpackFinal: async config => {
-    config.module.rules.push({
-      test: /\.mjs$/,
-      include: /node_modules/,
-      type: 'javascript/auto'
-    });
-    config.resolve.alias['./useMailingListForm'] = require.resolve('../src/__mocks__/useMailingListForm.mock.js');
-    return config;
-  },
+const config = {
   framework: {
     name: '@storybook/react-webpack5',
     options: {
       builder: {
-        fsCache: true
-      }
-    }
+        fsCache: true,
+      },
+    },
   },
-  docsPage: {
-    docs: 'automatic'
-  },
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.(tsx|js)'],
+  addons: [
+    '@storybook/addon-essentials',
+    '@storybook/addon-a11y',
+    'storybook-addon-pseudo-states',
+    '@storybook/addon-interactions',
+  ],
   docs: {
-    docsPage: 'automatic'
-  }
+    autodocs: true,
+  },
+  features: {
+    interactionsDebugger: true,
+  },
+  staticDirs: ['./static'],
+  webpackFinal: async (config) => {
+    config.module.rules.push({
+      test: /\.mjs$/,
+      include: /node_modules/,
+      type: 'javascript/auto',
+    });
+    config.resolve.alias['./useMailingListForm'] = require.resolve(
+      '../src/__mocks__/useMailingListForm.mock.js'
+    );
+    return config;
+  },
 };
+
+export default config;
